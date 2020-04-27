6fa96438272810ff7cafe8a6ff030e03e08f5925
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2020-04-27 11:55:44 +0300

    (fix) Menu. Теперь работает как надо - список раскрывается.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ const BaseLayout = () => (
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="navbar-collapse" id="navbarNavAltMarkup">
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             <a className="nav-item nav-link" href="/">CUSTOMERS</a>
             <a className="nav-item nav-link" href="/customer/">CREATE CUSTOMER</a>
