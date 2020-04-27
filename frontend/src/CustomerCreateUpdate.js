790b33d8e46870f20f478a318c8baf2e8ab117e7
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2020-04-27 09:50:46 +0300

    (clear) CustomerCreateUpdate. Удаление нерабочего закомментированного ранее кода.

--- frontend/src/CustomerCreateUpdate.js
+++ frontend/src/CustomerCreateUpdate.js
@@ -22,60 +22,19 @@ class CustomerCreateUpdate extends Component {
         if (params && params.pk) {
             window.alert("componentDidMount - if")
             customersService.getCustomer(params.pk).then((c)=> {
-                // this.refs.firstName.value = c.first_name;
-                // this.refs.lastName.value = c.last_name;
-                // this.refs.email.value = c.email;
-                // this.refs.phone.value = c.phone;
-                // this.refs.address.value = c.address;
-                // this.refs.description.value = c.description;
-
-                // this.firstName = c.first_name;
-                // this.lastName = c.last_name;
-                // this.email = c.email;
-                // this.phone = c.phone;
-                // this.address = c.address;
-                // this.description = c.description;
-
                 this.refFirstName.current.value = c.first_name;
                 this.refLastName.current.value = c.last_name;
                 this.refEmail.current.value = c.email;
                 this.refPhone.current.value = c.phone;
                 this.refAddress.current.value = c.address;
                 this.refDescription.current.value = c.description;
-
-                // this.setFirstName(c);
-                // this.setLastName(c);
-                // this.setEmail(c);
-                // this.setPhone(c);
-                // this.setAddress(c);
-                // this.setDescription(c);
             })
         }
     }
 
     handleCreate() {
         // window.alert("handleCreate")
-        // this.setFirstName();
-        // this.setLastName();
-        // this.setEmail();
-        // this.setPhone();
-        // this.setAddress();
-        // this.setDescription();
         customersService.createCustomer({
-            // "first_name": this.refs.firstName.value,
-            // "last_name": this.refs.lastName.value,
-            // "email": this.refs.email.value,
-            // "phone": this.refs.phone.value,
-            // "address": this.refs.address.value,
-            // "description": this.refs.description.value
-
-            // "first_name": this.firstName,
-            // "last_name": this.lastName,
-            // "email": this.email,
-            // "phone": this.phone,
-            // "address": this.address,
-            // "description": this.description
-
             "first_name": this.refFirstName.current.value,
             "last_name": this.refLastName.current.value,
             "email": this.refEmail.current.value,
@@ -91,20 +50,6 @@ class CustomerCreateUpdate extends Component {
     handleUpdate(pk) {
         customersService.updateCustomer({
             "pk": pk,
-            // "first_name": this.refs.firstName.value,
-            // "last_name": this.refs.lastName.value,
-            // "email": this.refs.email.value,
-            // "phone": this.refs.phone.value,
-            // "address": this.refs.address.value,
-            // "description": this.refs.description.value
-
-            // "first_name": this.firstName,
-            // "last_name": this.lastName,
-            // "email": this.email,
-            // "phone": this.phone,
-            // "address": this.address,
-            // "description": this.description
-
             "first_name": this.refFirstName.current.value,
             "last_name": this.refLastName.current.value,
             "email": this.refEmail.current.value,
@@ -129,29 +74,6 @@ class CustomerCreateUpdate extends Component {
         }
         event.preventDefault();
     }
-
-
-    // setFirstName = element => {
-    //     window.alert(element)
-    //     this.firstName = element;
-    //     window.alert(this.firstName)
-    // };
-    // setLastName = element => {
-    //     this.lastName = element;
-    // };
-    // setEmail = element => {
-    //     this.email = element;
-    // };
-    // setPhone = element => {
-    //     this.phone = element;
-    // };
-    // setAddress = element => {
-    //     this.address = element;
-    // };
-    // setDescription = element => {
-    //     this.description = element;
-    // };
-
 
     render() {
         return (
@@ -179,58 +101,6 @@ class CustomerCreateUpdate extends Component {
               </div>
           </form>
         );
-    // render() {
-    //     return (
-    //       <form onSubmit={this.handleSubmit}>
-    //           <div className="form-group">
-    //               <label>First Name: </label>
-    //               <input className="form-control" type="text" ref={this.setFirstName} />
-    //
-    //               <label>Last Name: </label>
-    //               <input className="form-control" type="text" ref={this.setLastName} />
-    //
-    //               <label>Email: </label>
-    //               <input className="form-control" type="text" ref={this.setEmail} />
-    //
-    //               <label>Phone: </label>
-    //               <input className="form-control" type="text" ref={this.setPhone} />
-    //
-    //               <label>Address: </label>
-    //               <input className="form-control" type="text" ref={this.setAddress} />
-    //
-    //               <label>Description: </label>
-    //               <textarea className="form-control" ref={this.setDescription}></textarea>
-    //
-    //               <input className="btn btn-primary" type="submit" value="Submit" />
-    //           </div>
-    //       </form>
-    //     );
-    // render() {
-    //     return (
-    //       <form onSubmit={this.handleSubmit}>
-    //           <div className="form-group">
-    //               <label>First Name: </label>
-    //               <input className="form-control" type="text" ref="first_name" />
-    //
-    //               <label>Last Name: </label>
-    //               <input className="form-control" type="text" ref="last_name" />
-    //
-    //               <label>Email: </label>
-    //               <input className="form-control" type="text" ref="email" />
-    //
-    //               <label>Phone: </label>
-    //               <input className="form-control" type="text" ref="phone" />
-    //
-    //               <label>Address: </label>
-    //               <input className="form-control" type="text" ref="address" />
-    //
-    //               <label>Description: </label>
-    //               <textarea className="form-control" ref="description"></textarea>
-    //
-    //               <input className="btn btn-primary" type="submit" value="Submit" />
-    //           </div>
-    //       </form>
-    //     );
     }
 }
 export default CustomerCreateUpdate;
