0bfcfa2c09fd6ae67731cce809386fe607ba9213
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2020-05-21 14:26:58 +0300

    (+) Тест views - class HomeTestCase(TestCase). Для url ''.

---
+++ backend/customers/tests/test_views/home.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.test.client import Client
+from rest_framework import status
+from django.shortcuts import render
+
+
+# Create your tests here.
+class HomeTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # ======================================================================
+
+    def test_get(self):
+        client = Client()
+        response = client.get('')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        request = response.context['request']
+        response_content_expected = render(request, 'customers/home.html').content
+        self.assertEquals(response.content, response_content_expected)
+
+    # ======================================================================
