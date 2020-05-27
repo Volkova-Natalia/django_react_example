27e502e05d11e2389601d2ab1ab5d11ce30aff1b
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2020-05-27 15:35:00 +0300

    (+) clean PUT тесты views - class CustomersDetailTestCase(TestCase). Для url: 'api/customers/<int:pk>'
    (!)
    - Не знаю, как лучше - разные тесты на каждое поле (как сейчас) или один тест на все поля (как закомментированная функция def test_put_fields_clean(self))
    
    (m) При создании исходных записей в базе теперь заполняются все поля. Соответственно и проверяются в GET тесте тоже все поля.

--- backend/customers/tests/test_views/customers_detail.py
+++ backend/customers/tests/test_views/customers_detail.py
@@ -3,6 +3,7 @@ from django.test.client import Client
 from rest_framework import status
 
 from customers.models import Customer
+from customers.serializers import CustomerSerializer
 
 import math
 
@@ -30,6 +31,10 @@ class CustomersDetailTestCase(TestCase):
             Customer(
                 first_name='first_name_' + str(i),
                 last_name='last_name_' + str(i),
+                email='email_' + str(i) + '@email.com',
+                phone='phone_' + str(i),
+                address='address_' + str(i),
+                description='description_' + str(i),
             ).save()
 
     def tearDown(self):
@@ -51,8 +56,72 @@ class CustomersDetailTestCase(TestCase):
             customer = Customer.objects.get(id=id_customer)
             self.assertEquals(getattr(customer, 'first_name'), 'first_name_' + str(id_customer-1))
             self.assertEquals(getattr(customer, 'last_name'), 'last_name_' + str(id_customer-1))
+            self.assertEquals(getattr(customer, 'email'), 'email_' + str(id_customer-1) + '@email.com')
+            self.assertEquals(getattr(customer, 'phone'), 'phone_' + str(id_customer-1))
+            self.assertEquals(getattr(customer, 'address'), 'address_' + str(id_customer-1))
+            self.assertEquals(getattr(customer, 'description'), 'description_' + str(id_customer-1))
 
     # ----- PUT -----
+
+    def _test_put_field_clean(self, client, id_customer, field, field_value_new=None):
+        customer = Customer.objects.get(id=id_customer)
+        data_put = {field: getattr(customer, field) for field in CustomerSerializer.Meta.fields}
+        if field_value_new:
+            data_put[field] = field_value_new
+        else:
+            data_put[field] = data_put[field] + '_put'
+        # print('\ndata_put', data_put)
+        response = client.put(
+            path=self.url_base + str(id_customer),
+            data=data_put,
+            content_type='application/json'
+        )
+        # print('\nresponse  ', response)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        customer = Customer.objects.get(id=id_customer)
+        for field in CustomerSerializer.Meta.fields:
+            self.assertEquals(getattr(customer, field), data_put[field])
+
+    # def test_put_fields_clean(self):
+    #     client = Client()
+    #     for id_customer in self.test_id_customers_clear:
+    #         self._test_put_field_clean(client, id_customer, 'first_name')
+    #         self._test_put_field_clean(client, id_customer, 'last_name')
+    #         self._test_put_field_clean(client, id_customer, 'email', 'email_' + str(id_customer) + '_put' + '@email.com')
+    #         self._test_put_field_clean(client, id_customer, 'phone')
+    #         self._test_put_field_clean(client, id_customer, 'address')
+    #         self._test_put_field_clean(client, id_customer, 'description')
+
+    def test_put_first_name_clean(self):
+        client = Client()
+        for id_customer in self.test_id_customers_clear:
+            self._test_put_field_clean(client, id_customer, 'first_name')
+
+    def test_put_last_name_clean(self):
+        client = Client()
+        for id_customer in self.test_id_customers_clear:
+            self._test_put_field_clean(client, id_customer, 'last_name')
+
+    def test_put_email_clean(self):
+        client = Client()
+        for id_customer in self.test_id_customers_clear:
+            self._test_put_field_clean(client, id_customer, 'email', 'email_' + str(id_customer) + '_put' + '@email.com')
+
+    def test_put_phone_clean(self):
+        client = Client()
+        for id_customer in self.test_id_customers_clear:
+            self._test_put_field_clean(client, id_customer, 'phone')
+
+    def test_put_address_clean(self):
+        client = Client()
+        for id_customer in self.test_id_customers_clear:
+            self._test_put_field_clean(client, id_customer, 'address')
+
+    def test_put_description_clean(self):
+        client = Client()
+        for id_customer in self.test_id_customers_clear:
+            self._test_put_field_clean(client, id_customer, 'description')
 
     # ----- DELETE -----
 
